e9fe38d468279e2bd1422d92812e0c41f1f5fa4f
chin31github <tclin.lin@gmail.com>
2025-01-13 15:17:07 +0800

    Add files via upload

--- student_assignment.py
+++ student_assignment.py
@@ -10,6 +10,32 @@ gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature']
+    )
+    match = re.search(r'(\d{4})年台灣(\d{1,2})月', question)
+    if match:
+        year = match.group(1)
+        month = match.group(2).zfill(2) 
+        #print(year)
+        #print(month)
+        prompt = f"列出{year}年台灣{month}月的所有紀念日，並以JSON格式呈現，每個紀念日包含日期和名稱，例如：{{'date': '年份-月份-日期', 'name': '紀念日名稱'}}。"
+        response = llm.invoke(prompt)
+ 
+        json_parser = JsonOutputParser()
+        json_output = json_parser.invoke(response)
+        result = {"Result": json_output}
+    else:
+        result = {"Result": []}
+ 
+ 
+    print(result)
+    return json.dumps(result, ensure_ascii=False, indent=2)
     pass
     
 def generate_hw02(question):
